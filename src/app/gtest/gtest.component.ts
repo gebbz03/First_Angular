d15360f9a8b46bb89976d6ce59b63c6738e11011
Gebb <gebb.freelancer@gmail.com>
2018-09-23 11:58:59 +0800

    Second commit

---
+++ src/app/gtest/gtest.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gtest',
+  templateUrl: './gtest.component.html',
+  styleUrls: ['./gtest.component.scss']
+})
+export class GtestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
